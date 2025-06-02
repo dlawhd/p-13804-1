ddeb7c32ae71e449e064b7c5b6384052dc383251
임종현 <whdvkf5139@naver.com>
2025-06-02 10:37:28 +0900

    1~8 다시!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        lab1();
     }
-}g
+
+    private static void  lab1(){
+        String input = "등록\n나의 죽음";
+        Scanner scanner = new Scanner(input);
+
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+
+        System.out.println(cmd);
+        System.out.println(content);
+
+    }
+}
